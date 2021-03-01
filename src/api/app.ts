3fcabb1c3b641b1af9fa2667acb0f918fee93d57
lucas-araujo <andrelucassaaraujo@gmail.com>
2021-03-01 15:48:05 -0400

    chore: add express

---
+++ src/api/app.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import 'reflect-metadata'
+import './database/index'
+
+import express, { json } from 'express';
+
+import {router} from './routes/index.routes'
+
+const app = express();
+app.use(json())
+
+app.use(router);
+
+export {app}
