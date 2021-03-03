e7995e64c6cc0d30665136f03d0c266dc2a383fb
lucas-araujo <andrelucassaaraujo@gmail.com>
2021-03-03 12:06:13 -0400

    refactor: fix import

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,7 +4,7 @@ import './database/index'
 
 import express, { json } from 'express';
 
-import {router} from './routes/index.routes'
+import {router} from '../routes/index.routes'
 
 const app = express();
 app.use(json())
