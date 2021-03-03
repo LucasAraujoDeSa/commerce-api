d391239bd1a086c176d45c512aab0545b22892ea
lucas-araujo <andrelucassaaraujo@gmail.com>
2021-03-03 12:32:03 -0400

    refactor: make changes to production

--- ormconfig.js
+++ ormconfig.js
@@ -3,15 +3,21 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: true,
   migrations: [
+    process.env.NODE_ENV == 'dev' ?
+    "./src/api/database/migrations/*.ts"
+    :
     "./dist/api/database/migrations/*.js"
   ],
   entities: [
+    process.env.NODE_ENV == 'dev' ?
+    "./src/api/Entitie/*.ts"
+    :
     "./dist/api/Entitie/*.js"
   ],
   cli: {
     migrationsDir: "./src/api/database/migrations"
   },
   extra: {
-    ssl: false
+    ssl: process.env.NODE_ENV == 'dev' ? false : {"rejectUnauthorized":false}
   }
 }
