624cd384e501d70e9c0d34d092371958f7f741e5
Gabriel <=>
2021-03-03 17:43:48 -0300

    feat: add product-repository

---
+++ src/api/repositories/product-repository.ts
@@ -0,0 +1,25 @@
+import { getRepository, Repository } from 'typeorm';
+import { IProduct } from '../ports/products/product';
+import { Product } from '../Entitie/Product';
+
+export class ProductRepository {
+  private readonly ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  async create(data: IProduct): Promise<Product> {
+    const product = this.ormRepository.create(data);
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  async find(): Promise<Product[]> {
+    const products = await this.ormRepository.find();
+
+    return products;
+  }
+}
