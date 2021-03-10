1c64210fd3fbb29e4c6478a62a70291429ec2e7a
Gabriel <=>
2021-03-10 13:07:59 -0300

    refactor: add implementation interface

--- src/api/repositories/user-repository.ts
+++ src/api/repositories/user-repository.ts
@@ -1,17 +1,24 @@
+import { IUserRepository } from './ports/IUser-Repository';
 import { getRepository, Repository } from 'typeorm';
 import { IUser } from '../ports/users/user';
 import { User } from '../Entitie/User';
 
-export class UserRepository {
+export class UserRepository implements IUserRepository {
   private readonly ormRepository: Repository<User>;
 
   constructor() {
     this.ormRepository = getRepository(User);
   }
 
-  async findByEmail(email: string): Promise<User> {
+  async findById(id: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne(id);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
     const user = await this.ormRepository.findOne({
-      where: email,
+      where: { email },
     });
 
     return user;
@@ -19,10 +26,12 @@ export class UserRepository {
 
   async create(data: IUser): Promise<User> {
     const user = this.ormRepository.create(data);
-
     await this.ormRepository.save(user);
-
     return user;
+  }
+
+  async save(data: IUser): Promise<User> {
+    return await this.ormRepository.save(data);
   }
 
   async find(): Promise<User[]> {
