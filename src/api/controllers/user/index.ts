f2d5ee9f2e250dd5d33d7c5c65b7f183a48a7e77
Gabriel <=>
2021-03-10 13:22:19 -0300

    feat: add imports user's controller

---
+++ src/api/controllers/user/index.ts
@@ -0,0 +1,5 @@
+export * from './create-user-controller';
+export * from './find-users-controller';
+export * from './reset-password-controller';
+export * from './forgot-password-email-controller';
+export * from './auth-user-controller';
