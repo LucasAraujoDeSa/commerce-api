45611071985059a8f5b411ee802f164d5d04d0e4
Gabriel <=>
2021-03-10 13:23:31 -0300

    refactor: add new routes to user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
-import { CreateUserController } from '@/api/controllers/user/create-user-controller';
+import {
+  CreateUserController,
+  FindUsersController,
+  ResetPasswordController,
+  ForgotPasswordController,
+  AuthUserController,
+} from '@/api/controllers/user/index';
 
 const userRouter = Router();
 
+const authUserController = new AuthUserController();
 const createUserController = new CreateUserController();
+const resetPasswordController = new ResetPasswordController();
+const forgtPasswordController = new ForgotPasswordController();
+const findUsersController = new FindUsersController();
 
 userRouter.post('/', createUserController.create);
-//userRouter.get('/', findProductController.find);
+userRouter.post('/reset', resetPasswordController.create);
+userRouter.post('/forgot', forgtPasswordController.create);
+userRouter.get('/', findUsersController.find);
+userRouter.post('/session', authUserController.create);
 
 export { userRouter };
