d8eff3122128a2197107182c172baea52508b5b8
Gabriel <=>
2021-03-10 13:23:47 -0300

    refactor: add dependecy injector

--- src/api/services/products-service/create-product-service.ts
+++ src/api/services/products-service/create-product-service.ts
@@ -1,9 +1,13 @@
 import { IProduct } from './../../ports/products/product';
+import { inject, injectable } from 'tsyringe';
 import { Product } from '../../entitie/Product';
 import { ProductRepository } from '../../repositories/product-repository';
 
+@injectable()
 export class CreateProductService {
-  constructor(private productRepository: ProductRepository) {}
+  constructor(
+    @inject('ProductRepository') private productRepository: ProductRepository,
+  ) {}
 
   public async execute(data: IProduct): Promise<Product> {
     const product = await this.productRepository.create(data);
